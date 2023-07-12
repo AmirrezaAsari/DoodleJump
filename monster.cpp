ef452f4daea7cfc415f050c4340ccffae972a193
unknown <arya.khoshnava@gmail.com>
2023-07-12 10:12:39 +0330

    Add monster.cpp

---
+++ monster.cpp
@@ -0,0 +1,15 @@
+#include "monster.h"
+#include <cstdlib>
+#include <QTime>
+#include <ctime>
+
+monster::monster()
+{
+
+}
+void monster::move(double dy){
+    setPos(x(),y()-dy);
+}
+void monster::setmonster(){
+
+}
