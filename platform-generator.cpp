2b05d04a0b857519d0b8219a0a978f1103ad4e14
unknown <amirreza.asari2@gmail.com>
2023-07-13 02:08:23 +0330

    added monster generation

--- platform-generator.cpp
+++ platform-generator.cpp
@@ -1,5 +1,9 @@
 #include "platform-generator.h"
 #define PLATFORM_NUM 3
+#define MONSTER_NUM 3
+#define MONSTER_1 0
+#define MONSTER_2 1
+#define MONSTER_3 2
 
 platform* genplat(){
     int plat_tp;
@@ -22,5 +26,25 @@ platform* genplat(){
 platform2* genplat2(){
     platform2 *ptr = new platform2;
     return ptr;
+}
 
+monster* genmonst(platform* p){
+    int mon_tp;
+    monster *ptr;
+
+    mon_tp = rand() % MONSTER_NUM ;
+    switch (mon_tp) {
+    case MONSTER_1:
+        ptr = new monster1(p);
+        break;
+    case MONSTER_2:
+        ptr = new monster2;
+        ptr->setmonster();
+        break;
+    case MONSTER_3:
+        ptr = new monster3;
+        ptr->setmonster();
+        break;
+    }
+    return ptr;
 }
--- platform-generator.h
+++ platform-generator.h
@@ -5,8 +5,12 @@
 #include "platform1.h"
 #include "platform2.h"
 #include "platform3.h"
+#include "monster.h"
+#include "monster1.h"
+#include "monster2.h"
+#include "monster3.h"
 
 platform* genplat();
 platform2* genplat2();
-
+monster* genmonst(platform* p);
 #endif // PLATFORMGENERATOR_H
