9a1d0a0530e08f023d7bd30551c5baf54cb0975b
unknown <amirreza.asari2@gmail.com>
2023-07-13 09:37:18 +0330

    mainwindow

--- mainwindow.h
+++ mainwindow.h
@@ -2,10 +2,23 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QKeyEvent>
+#include <QGraphicsScene>
+#include <QGraphicsPixmapItem>
+#include <QTimer>
+#include <vector>
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
+#include "basics.h"
+#include "trampo.h"
+#include "platform-generator.h"
+#include "bullet.h"
+#include "jetpack.h"
+#include "shield.h"
+
+using namespace std;
+namespace Ui {
+class MainWindow;
+}
 
 class MainWindow : public QMainWindow
 {
@@ -16,12 +29,43 @@ public:
     ~MainWindow();
     void generate();
     void sethard();
+    void timestop();
     void set();
 
 public slots:
     void jump();
+    void setPic();
+    void Gameover();
+    void invincible_end();
+    virtual void keyPressEvent(QKeyEvent *e);
+    void mousePressEvent(QMouseEvent *e);
+
+private slots:
+    void on_Pausebt_clicked();
+    void on_Resumebt_clicked();
+    void on_playButton_clicked();
+    void on_menuButton_clicked();
+    void on_Normal_clicked();
+    void on_Multiplayer_clicked();
+
 
 private:
     Ui::MainWindow *ui;
+    shield *sd;
+    QGraphicsScene *scene;
+    QGraphicsPixmapItem *player,*pausebg,*line;
+    QTimer *timer,*b_timer;
+    bullet *b;
+    jetpack *jet;
+    trampo *trp;
+    vector<platform*> plf_ptr;
+    vector<monster*> mon_ptr;
+    vector<platform2*>plf2_ptr;
+    int count=0,t=0,plf2 = 0,score = 0,game_mode;
+    unsigned long a = 0;
+    double dy,dx=0;
+    bool fly_md,invin_md;
+    bool jet_est = false,shield_est = false;
 };
+
 #endif // MAINWINDOW_H
